e0d7d744215f808cba7121197eda0fd4051e3be0
MarcBordes <marpa.2@hotmail.com>
2023-09-26 00:36:12 +0200

    Actualizados ficheros - ELias & Marc

--- PHP/Sesion1/gestio_ventes.php
+++ PHP/Sesion1/gestio_ventes.php
@@ -49,61 +49,105 @@ function compra_clients($id_cliente, $dicc)
     return array_unique($productos);
 }
 //EJERCICIO 2 PERO NO SE LE TIENE QUE PASAR NADA, LO HACE TODO AUTOMÁTICO.
-function compra_clientes($dicc)
+function compra_clientes($dicc, $clave)
 {
-    $diccionarioConClientes = array();
-
+    $diccionarioConClientes = array([]);
     foreach ($dicc as $prod) {
         foreach ($prod as $diccInterno) {
-            $clave = $diccInterno["Cust_ID"];
-            if (array_key_exists($clave, $diccionarioConClientes)) {
+            if ($diccInterno["Cust_ID"] == $clave) {
                 // Si la clave existe, agrega el elemento a la lista existente
                 $diccionarioConClientes[$clave][] = $diccInterno;
-            } else {
-                // Si la clave no existe, crea una nueva clave con una lista que contiene el elemento
-                $diccionarioConClientes[$clave] = array($diccInterno);
             }
-
         }
     }
     return $diccionarioConClientes;
 }
 
 //EJERCICIO 3
-function guardar_dades($dicc){
+function guardar_dades($dicc)
+{
 
     $file = 'ventas.json';
     file_put_contents($file, json_encode($dicc));
-}   
-
-//EJERCICIO 4
-function afegeix_compra ($dicc_ventas,$compra) {
-    //FALTA QUE SE LE PASE LOS DATOS QUE QUEREMOS QUE SE GUARDE. - Creo que los atributos tienen que ser del mismo nombre que en el fichero - Cust_ID , product ...
-    $miDato = array(
-        "producto" => $compra[0],
-        "pais" => $compra[1],
-        "fecha" => $compra[2],
-        "cantidad" => intval($compra[3]), // Convertir a entero
-        "precio" => floatval($compra[4]), // Convertir a flotante
-        "cliente" => $compra[5]
-    );
-    $dicc_ventas[$compra["producto"]] = $miDato;
-    guardar_dades($dicc_ventas);
 }
 
-function borrar_compra($dicc,$compra) {
-    $listaProductos = $dicc[$compra["producto"]];
+//EJERCICIO 4
+function afegeix_compra($dicc_ventas, $compra)
+{
+    //FALTA QUE SE LE PASE LOS DATOS QUE QUEREMOS QUE SE GUARDE.
+
+    $miDato = array(
+        "country" => $compra[1],
+        "date" => $compra[2],
+        "quantity" => $compra[3],
+        "amount" => $compra[4],
+        "card" => $compra[5],
+        "Cust_ID" => $compra[6]
+    );
+    if (array_key_exists($compra[0], $dicc_ventas)) {
+        // Si la clave existe, agrega el elemento a la lista existente
+        $dicc_ventas[$compra[0]][] = $miDato;
+    } else {
+        // Si la clave no existe, crea una nueva clave con una lista que contiene el elemento
+        $dicc_ventas[$compra[0]] = array($miDato);
+    }
+
+    guardar_dades($dicc_ventas);
+
+}
+
+function borrar_compra($dicc, $compra)
+{
+    $listaProductos = $dicc[$compra[0]];
     foreach ($listaProductos as $indice => $lista) {
         $resultado = array_diff($lista, $compra);
-        
-        if (empty($resultado)) { 
+
+        if (empty($resultado)) {
             //ELIMINO EL DATO DEL INDICE DONDE LO HEMOS ENCONTRADO.
-            unset($listaProductos[$indice]);
+            unset($dicc[$compra[0]][$indice]);
         }
     }
 }
 
 
+
+
+
+function importar_dades($archivo_csv)
+{
+    
+    //En primer lugar se tiene que leer los datos del fichero.
+    $file = fopen($archivo_csv, 'r');
+    $datos = array();
+    $first_line = fgetcsv($file);
+
+    while ($row = fgetcsv($file)) {
+        if (!array_key_exists($row[0], $datos)) {
+            $datos[$row[0]] = [];
+        }
+
+
+            
+        $miDato = array(
+            "country" => $row[1],
+            "date" => (new DateTime($row[2]))->format('Y-m-d'),
+            "quantity" => intval($row[3]),
+            "amount" => floatval($row[4]), 
+            "card" => $row[5],
+            "Cust_ID" => $row[6]
+        );
+
+
+        $datos[$row[0]][] = $miDato;
+    }
+    return $datos;
+
+}
+
+function carregar_dades ($dicc) {
+    $file = 'ventas.json';
+    file_put_contents($file, json_encode($dicc));
+}
 
 //Funcion para mostrar el diccionario por HTML
 function print_dicc($dicc)
@@ -130,20 +174,6 @@ function print_dicc($dicc)
         echo "</div>";
     }
 
-    //EJERCICIO 2
-
-    /*  echo "<h1> COMPRA POR CLIENTE </h1><br>";
-
-      foreach ($dicc as $producto => $ventas) {
-          echo "<li><b> $producto </b></li>";
-          foreach ($ventas as $venta) {
-              foreach ($venta as $categoria => $result ) {
-              echo "$categoria --> $result <br>";
-              }
-              echo "<br>";
-          } 
-      }
-      */
 }
 //EJERCICIO 2 
 function print_clientes($dicc)
@@ -162,18 +192,24 @@ function print_clientes($dicc)
 }
 
 
-
-
 $fichero = "sales_2008-2011.csv";
 $idcliente = "Cust_8";
 
-$dicc_ventas = importar_dades0($fichero);
+$dicc_ventas = importar_dades($fichero);
 guardar_dades($dicc_ventas);
-//print_r($dicc_ventas);
-//print_dicc($dicc_ventas);
-//print_r(compra_clients($idcliente, $dicc_ventas));
-//print_clientes(compra_clientes($dicc_ventas));
-print_r(compra_clientes($dicc_ventas));
+
+$compra = array("prod_5", "España", "2008-12-12", 1, 3, "N", "Cust_8");
+//afegeix_compra($dicc_ventas, $compra);
+
+$compraBorar = array("prod_3", "China", "2009-04-10", "2", "160", "N", "Cust_2");
+//borrar_compra($dicc_ventas,$compraBorar);
+//print_r(compra_clientes($dicc_ventas));
+
+//compra_clientes($dicc_ventas, $idcliente);
+
 //guardar_dades(compra_clientes($dicc_ventas));
+//var_dump($dicc_ventas);
+
+print_dicc($dicc_ventas);
 
 ?>
