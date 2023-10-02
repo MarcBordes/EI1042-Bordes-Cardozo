7dfd869663d98f6ff86ba6bbef01d88a203caab1
MarcBordes <marpa.2@hotmail.com>
2023-10-03 00:14:34 +0200

    prueba JSON

--- PHP/B_1/partials/tablas.php
+++ PHP/B_1/partials/tablas.php
@@ -14,13 +14,8 @@
     <tbody>
       <?php
         // Ejemplo de datos de cursos
-        $cursos = array(
-          array("C001", "Introducción a la Programación", 30, 35, 150),
-          array("C002", "Matematicas Avanzadas", 25, 8, 180),
-          array("C003", "Idioma: Italiano", 20, 70, 120),
-          array("C004", "Python", 35, 15, 200),
-          array("C005", "Arte Digital", 18, 5, 160)
-        );
+        $json_data = file_get_contents('datos_cursos.json');
+        $cursos = json_decode($json_data, true);
 
         foreach ($cursos as $curso) {
           echo "<tr>";
