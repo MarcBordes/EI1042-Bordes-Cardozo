7dbb966c2f88cca9dc5ef7597b09d486e1c426a3
MarcBordes <marpa.2@hotmail.com>
2023-10-03 20:27:32 +0200

    home modificado

--- PHP/B_1/partials/home.php
+++ PHP/B_1/partials/home.php
@@ -1,6 +1,37 @@
 <main>
-	<h2>Bem-vindo à melhor escola do mundo</h2>
+	<h2>Bienvenido a la Future Mind School</h2>
+	<p>
+		En esta página encontrarás toda la información relevante sobre nuestro centro educativo!!!
+	</p>
+	<br>
 
-	
+	<div class="contenedor">
+
+		<h2>Próximos Eventos: </h2>
+
+		<ul>
+			<li>
+				<span>22 de octubre, 2023</span>
+				<h2>Conferencia Parental</h2>
+				<p>Una sesión informativa para padres sobre el progreso académico y actividades extracurriculares.</p>
+			</li>
+
+			<li>
+				<span>5 de noviembre, 2023</span>
+				<h2>Feria de Ciencias</h2>
+				<p>Los estudiantes presentarán proyectos científicos fascinantes. ¡Todos están invitados!</p>
+			</li>
+
+			<li>
+				<span>12 de diciembre, 2023</span>
+				<h2>Concierto de Navidad</h2>
+				<p>Disfruta de una noche llena de música y alegría en nuestro concierto navideño.</p>
+			</li>
+
+		</ul>
+
+		<img src="../media/science.jpg" alt="imagen ciencia">
+
+	</div>
 
 </main>
