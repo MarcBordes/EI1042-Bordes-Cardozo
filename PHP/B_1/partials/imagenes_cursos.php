7d684441044511063010c90cd5083b1736ec6952
MarcBordes <marpa.2@hotmail.com>
2023-11-12 18:22:31 +0100

    ejemplos añadidos

--- PHP/B_1/partials/imagenes_cursos.php
+++ PHP/B_1/partials/imagenes_cursos.php
@@ -1,6 +1,6 @@
     <?php
     $imageDir = __DIR__ . '/../media/fotos';
-    $images = glob($imageDir . '/{*.jpg,*.png}', GLOB_BRACE);
+    $images = glob($imageDir . '/{*.jpg,*.png,*.jpeg}', GLOB_BRACE);
     shuffle($images);
     sizeof($images) > 9 ? $slice =  9 : $slice = sizeof($images);
     $randomImages = array_slice($images, 0, $slice );
