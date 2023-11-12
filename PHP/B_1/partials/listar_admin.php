3810daa35d8bbcacc9e5ba01aad4ae81be18b8ec
MarcBordes <marpa.2@hotmail.com>
2023-11-12 18:07:01 +0100

    modificar fixed

--- PHP/B_1/partials/listar_admin.php
+++ PHP/B_1/partials/listar_admin.php
@@ -10,7 +10,6 @@ if (file_exists($jsonFile)) {
     $cursos = json_decode($jsonContent, true);
 }
 
-echo '<form method="post" action="procesar_formulario.php">';
 echo '<table border="1">';
 echo '<tr>';
 echo '<th>Nombre del Curso</th>';
@@ -27,13 +26,15 @@ echo '<th>Acciones</th>';
 echo '</tr>';
 
 foreach ($cursos as $nombreCurso => $curso) {
+    echo '<form method="post" action="procesar_formulario.php">';
+
     echo '<tr>';
     echo '<td>' . $curso['nombre_actividad'] . '</td>';
     echo '<td><input type="number" name="alumnos_maximos" value="' . $curso['AlumnosMaximos'] . '" required maxlength="5" min="0"></td>';
     echo '<td><input type="number" name="plazas_vacantes" value="' . $curso['PlazasVacantes'] . '" required maxlength="5" min="0"></td>';
     echo '<td><input type="number" name="precio" value="' . $curso['Precio'] . '" required maxlength="5" min="0"></td>';
     echo '<td><input type="text" name="descripcion" value="' . $curso['Descripcion'] . '" style="width: 300px; height: 100px;" required maxlength="500"></td>';
-    echo '<td><input type="text" name="name_foto" id="name" class="item_requerid" value="' . $curso['NombreImagen'] .'"  size="20" maxlength="25" required></td>';
+    echo '<td><input type="text" name="name_foto" class="item_requerid" value="' . $curso['NombreImagen'] .'"  size="20" maxlength="25" required></td>';
     echo '<td><img style= "max-width: 500px; max-height: 200px;" src="' . $curso['fotoCliente'] . '" ></td>';
     //echo '<td><input type="text" name="foto_cliente" value="' . $curso['fotoCliente'] . '"></td>';
 
@@ -45,10 +46,11 @@ foreach ($cursos as $nombreCurso => $curso) {
     echo '<a class="btn-delete" href="?action=borrar&curso=' . $nombreCurso . '" " style="text-decoration: none;">Borrar</a>';
         echo '</td>';
     echo '</tr>';
+    echo '</form>';
+
 }
 
 echo '</table>';
-echo '</form>';
 
 echo '</main>';
 
