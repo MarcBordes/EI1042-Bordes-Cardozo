407655ee5ac56decc75028af89be37629002e84a
elias <al405647@uji.es>
2023-12-02 13:02:42 +0100

    firma_online está finalizado, lo he hecho un poco más bonito :)

---
+++ PHP/B_1/guardar_firma.php
@@ -0,0 +1,38 @@
+<?php
+
+$directorioDestino = "./media/firmas/";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    if (isset($_FILES['imagen'])) {
+        
+        // Mueve el archivo original al directorio de destino
+        $rutaCompletaOriginal = $directorioDestino . basename($_FILES['imagen']['name']);
+
+            if (isset($_POST['firma'])) {
+                $firmaBase64 = $_POST['firma'];
+
+                // Decodifica la cadena base64 y obtiene los datos binarios de la firma
+                $firmaBinaria = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $firmaBase64));
+
+                // nombre único para la firma
+                $nombreFirma = uniqid('firma_') . '.jpg';
+
+                $rutaCompletaFirma = $directorioDestino . $nombreFirma;
+                if (file_put_contents($rutaCompletaFirma, $firmaBinaria)) {
+                    echo json_encode(['success' => true, 'url' => $rutaCompletaFirma]);
+                } else {
+                    echo json_encode(['success' => false, 'message' => 'Error al guardar la firma']);
+                }
+            } else {
+                echo 'No se recibió la firma';
+            }
+
+    } else {
+        echo json_encode(['success' => false, 'message' => 'No se recibió la imagen original']);
+    }
+
+} else {
+    // La solicitud no es de tipo POST
+    echo json_encode(['success' => false, 'message' => 'Método de solicitud no permitido']);}
+?>
