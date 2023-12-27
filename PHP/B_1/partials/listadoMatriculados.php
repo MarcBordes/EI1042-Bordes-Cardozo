8707a063c2c8744b2b9f7a57c29d2c8452b22744
MarcBordes <marpa.2@hotmail.com>
2023-12-27 19:35:17 +0100

    listado matriculados funcional

--- PHP/B_1/partials/listadoMatriculados.php
+++ PHP/B_1/partials/listadoMatriculados.php
@@ -66,6 +66,7 @@
 
             // Función para cargar asincrónicamente la lista de alumnos matriculados de un curso específico
             function cargarAlumnosMatriculados(curso) {
+                // Obtener matriculados desde el archivo JSON
                 fetch('/recursos/matriculados.json')
                     .then(response => {
                         if (!response.ok) {
@@ -74,38 +75,76 @@
                         return response.json();
                     })
                     .then(data => {
-                        // Obtener la lista de alumnos matriculados para el curso seleccionado
-                        const alumnos = data[curso] || [];
+                        // Obtener la lista de usuarios matriculados para el curso seleccionado
+                        const alumnosIds = data[curso] || [];
+                        console.log(alumnosIds);
 
-                        // Llenar el contenedor de alumnos matriculados
-                        const listaAlumnos = document.getElementById('listaAlumnos');
-                        listaAlumnos.innerHTML = ''; // Limpiar la lista antes de agregar nuevos elementos
 
-                        if (alumnos.length > 0) {
-                            const titulo = document.createElement('h3');
-                            titulo.textContent = 'Alumnos Matriculados en ' + curso;
-                            listaAlumnos.appendChild(titulo);
+                        // Cargar usuarios desde el archivo CSV
+                        fetch('/recursos/seguro/users.csv')
+                            .then(response => response.text())
+                            .then(csvData => {
+                                // Convertir el CSV a un array de objetos
+                                const usuarios = parseCSV(csvData);
+                                console.log(csvData)
 
-                            const lista = document.createElement('ul');
-                            alumnos.forEach(alumno => {
-                                const item = document.createElement('li');
-                                const nombre = alumno.user_name || alumno.nom || '';
-                                const id = alumno.user_id || alumno.email || '';
-                                item.textContent = `Nombre: ${nombre} --> ID: ${id}`;
-                                lista.appendChild(item);
+
+                                // Filtrar usuarios por los IDs matriculados
+                                const matriculadosUsuarios = usuarios.filter(usuario => alumnosIds.includes(usuario.user_name.trim()));
+                                console.log(matriculadosUsuarios)
+
+                                // Llenar el contenedor de alumnos matriculados
+                                const listaAlumnos = document.getElementById('listaAlumnos');
+                                listaAlumnos.innerHTML = ''; // Limpiar la lista antes de agregar nuevos elementos
+
+                                if (matriculadosUsuarios.length > 0) {
+                                    const titulo = document.createElement('h3');
+                                    titulo.textContent = 'Alumnos Matriculados en ' + curso;
+                                    listaAlumnos.appendChild(titulo);
+
+                                    const lista = document.createElement('ul');
+                                    matriculadosUsuarios.forEach(usuario => {
+                                        const item = document.createElement('li');
+                                        item.textContent = `Nombre: ${usuario.user_name} --> ID: ${usuario.user_id}`;
+                                        lista.appendChild(item);
+                                    });
+
+                                    listaAlumnos.appendChild(lista);
+                                } else {
+                                    const mensaje = document.createElement('p');
+                                    mensaje.textContent = 'No hay alumnos matriculados en este curso.';
+                                    listaAlumnos.appendChild(mensaje);
+                                }
+                            })
+                            .catch(error => {
+                                console.error('Error al cargar usuarios:', error);
                             });
-
-                            listaAlumnos.appendChild(lista);
-                        } else {
-                            const mensaje = document.createElement('p');
-                            mensaje.textContent = 'No hay alumnos matriculados en este curso.';
-                            listaAlumnos.appendChild(mensaje);
-                        }
                     })
                     .catch(error => {
                         console.error('Error al cargar alumnos matriculados:', error);
                     });
             }
+
+            // Función para parsear el CSV y convertirlo en un array de objetos
+            function parseCSV(csvData) {
+                const lines = csvData.split('\n');
+                const headers = lines[0].split(',');
+                const result = [];
+
+                for (let i = 1; i < lines.length; i++) {
+                    const currentLine = lines[i].split(',');
+                    if (currentLine.length === headers.length) {
+                        const obj = {};
+                        for (let j = 0; j < headers.length; j++) {
+                            obj[headers[j].trim()] = currentLine[j].trim();
+                        }
+                        result.push(obj);
+                    }
+                }
+                
+                return result;
+            }
+
 
             // Cargar la lista de cursos al cargar la página
             window.onload = function () {
